36e9629c31b433d8059b6d26898cef5cb7537482
Nathan McCutchen <n8wm@icloud.com>
2019-10-08 17:08:59 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = /*8080;*/process.env.PORT;
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+  /**
+  socket.emit('request', ###); // emit an event to the socket
+  io.emit('broadcast', ###); // emit an event to all connected sockets
+  socket.on('reply', function(){ ### }); // listen to the event
+  **/
+
+  console.log("load");
+});
+
+app.use(express.static('static'));
+
+http.listen(port, () => console.log('Example app listening on port ' + port + '!'));
